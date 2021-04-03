0f42f3d81aaf88372f2504af71eca7dd8f61ad68
mikucloud <mikuwf@gmail.com>
2021-04-04 01:29:53 +0800

    Initial commit

---
+++ env.js
@@ -0,0 +1,11 @@
+window.settings = {
+  title: '可愛人',
+  description: 'powered by mikucloud',
+  host: 'https://app.cutecloud.org',
+  theme: {
+    sidebar: 'dark',
+    header: 'dark',
+    color: 'black'
+  },
+  background_url: ''//https://s3.ax1x.com/2021/01/31/yEgoNR.png
+}
